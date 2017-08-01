f3b2f4b05319cc5810f525204becd3072ae6010e
Stella Cannefax <stella@unity3d.com>
2017-08-01 14:38:23 -0500

    assets and project settings base

---
+++ Assets/Editor/CheckoutLatest.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+public static class CheckoutLatest
+{
+	public static void EditorXR ()
+	{
+		// if you want your Cloud Build target's branch to checkout a different EditorXR branch, 
+		// just change the branch variable here - this way we can have CI on any branch
+		const string branch = "development";
+		const string repoUrl = "https://github.com/Unity-Technologies/EditorVR";
+
+		string gitArguments = "lfs clone --recursive -b " + branch + " " + repoUrl;
+
+		UnityEngine.Debug.Log("running EditorXR checkout command:");
+		StartProcess(gitArguments);
+	}
+
+	static void StartProcess (string command)
+	{
+		UnityEngine.Debug.Log("git " + command);
+		ProcessStartInfo startInfo = new ProcessStartInfo("git", command);
+
+		startInfo.WorkingDirectory = Application.dataPath;
+		startInfo.UseShellExecute = false;
+		startInfo.RedirectStandardInput = false;
+		startInfo.RedirectStandardOutput = false;
+		startInfo.RedirectStandardError = false;
+
+		Process process = new Process();
+		process.StartInfo = startInfo;
+		process.Start();
+
+		process.WaitForExit();
+	}
+}
